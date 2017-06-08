d6a4fee9a92a88fd62957e1defffe28bef0c32b5
theallisonbrett <theallisonbrett@gmail.com>
2017-06-07 18:43:20 -0700

    Display average word length and beautify JS

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 function getText() {
-  $(".js-text-form").submit(function(event) {
-      event.preventDefault();
- var text = $(this).find('#user-text').val();
-handleText(text);
+    $(".js-text-form").submit(function(event) {
+        event.preventDefault();
+        var text = $(this).find('#user-text').val();
+        handleText(text);
 
-});
+    });
 
 }
 $(function() {
-  getText();
+    getText();
 });
 
 function handleText(text) {
-  var newText = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, " ").trim();
-  wordCount(newText);
+    var newText = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s+/g, " ").trim();
+    wordCount(newText);
 }
 
 function wordCount(newText) {
-var totalCount = newText.split(' ').length;
-$(".js-text-report").removeClass("hidden");
-$(".js-word-count").append(" " +totalCount);
-uniqueWords(newText);
+    var totalCount = newText.split(' ').length;
+    $(".js-text-report").removeClass("hidden");
+    $(".js-word-count").append(" " + totalCount);
+    uniqueWords(newText);
 }
 
 function uniqueWords(newText) {
- var array = newText.split(" ");
- var uniqueCounter = {};
+    var array = newText.split(" ");
+    var uniqueCounter = {};
 
-for (var i = 0; i < array.length; i++) {
-  if (uniqueCounter[array[i]] === undefined) {
-    uniqueCounter[array[i]] = 1;
-  }
-  else {
-    uniqueCounter[array[i]] += 1;
-  }
-}
- var uniqueWord = Object.values(uniqueCounter).length;
- $(".js-unique-word").append(" " + uniqueWord);
+    for (var i = 0; i < array.length; i++) {
+        if (uniqueCounter[array[i]] === undefined) {
+            uniqueCounter[array[i]] = 1;
+        } else {
+            uniqueCounter[array[i]] += 1;
+        }
+    }
+    var uniqueWord = Object.values(uniqueCounter).length;
+    $(".js-unique-word").append(" " + uniqueWord);
 
- averageWordLength(newText);
+    averageWordLength(newText);
 }
 
 function averageWordLength(newText) {
-  var array = newText.split(" ");
-  var lengthArray = [];
-for (var i = 0; i < array.length; i++) {
-     lengthArray.push(array[i].length);
-}
-var average = lengthArray.reduce(function(a,b){
-  return a + b/lengthArray.length;
-});
-console.log(average);
+    var array = newText.split(" ");
+    var lengthArray = [];
+    for (var i = 0; i < array.length; i++) {
+        lengthArray.push(array[i].length);
+    }
+    var average = lengthArray.reduce(function(a, b) {
+        return a + b / lengthArray.length;
+
+    });
+    $(".js-word-length").append(" " + average);
 }
